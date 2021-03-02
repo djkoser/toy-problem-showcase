9adb72b6c474c153c7c59ea14f9a821f461ab462
djkoser <koser.david@gmail.com>
2021-03-02 17:17:26 -0600

    Complete EvenAndOdd

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'; 
+
+class EvenAndOdd extends Component {
+  constructor (props) {
+    super(props);
+    
+    this.state={
+      evenArray: [],
+      oddArray: [],
+      userInput: ""
+    };
+    
+  };
+
+  evenAndOdd() {
+    let arrayCopy = this.state.userInput.split(",");
+    let evens = arrayCopy.filter(el=> el%2===0 ? true : false);
+    let odds = arrayCopy.filter(el=> el%2===0 ? false :true);
+
+    this.setState({
+      evenArray: evens,
+      oddArray:odds,
+      userInput: ""
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4>Evens And Odds</h4>
+        <input className="inputLine" onChange={(event) => this.setState({
+          userInput: event.target.value
+        })} value= {this.state.userInput}></input>
+        <button className="confirmationButton" onClick={()=>this.evenAndOdd()}> Submit</button>
+        <span className="resultsBox">Evens: {this.state.evenArray.join(', ')}</span>
+        <span className="resultsBox">Odds: {this.state.oddArray.join(', ')}</span>
+      </div>
+    )
+  }
+}
+
+export default EvenAndOdd
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class FilterObject extends Component {
+  constructor (props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p>Filter Object</p>
+    )
+  }
+}
+
+export default FilterObject
